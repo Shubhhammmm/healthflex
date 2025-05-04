91dcea2112ccbf8158dcde78057939b7d058c27f
shubham <s8982034020@gmail.com>
2025-05-04 15:56:34 +0530

    healthflex

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import { useReducer } from 'react';
 import { Link } from 'react-router-dom';
-import AddTimer from './AddTimer';
-import Category from './Category';
+import {AddTimer} from './AddTimer';
+import {Category} from './Category';
 
 const initialState = {
   timers: JSON.parse(localStorage.getItem('timers')) || [],
